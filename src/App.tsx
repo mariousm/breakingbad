48dbcc22d0baee2aa0bbd9fe149f860b45f3bf21
mariousm <mariousm7@gmail.com>
2020-08-20 10:12:50 +0100

    Solucionado el problema con la librería emotion, ejecutar npm i @emotion/styled @emotion/core

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React from "react";
+import styled from "@emotion/styled";
+
+// Definimos los estilos del componente
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+  const consultarAPI = () => {
+    console.log("consultando...");
+  }
+
   return (
-    <h1>Breaking Bad</h1>
+    <Div>
+      <Button onClick={consultarAPI}>Breaking Bad</Button>
+    </Div>
   );
 }
 
