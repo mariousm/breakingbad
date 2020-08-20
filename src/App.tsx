dded409f373f41a5bf40e04855454afe07961444
mariousm <mariousm7@gmail.com>
2020-08-20 17:46:32 +0100

    Finalizado primer ejemplo de consulta a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+// Librerías
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
+// Componentes
+import Frase from "./Components/Frase";
+// Interfaces
+import { IFrase } from "./Interfaces/IFrase";
 
 // Definimos los estilos del componente
 const Div = styled.div`
@@ -23,16 +28,38 @@ const Button = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
+  // Definimos el state de la frase
+  const [frase, guardarFrase] = useState<IFrase>({
+    quote: "",
+    author: "",
+  });
 
-  const consultarAPI = () => {
-    console.log("consultando...");
-  }
+  // Método que se ejecuta al pulsar el botón, en él consultamos a la API
+  const consultarAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  };
+
+  // Método que se ejecuta cuando el componente se carga o se produce un cambio en el state
+  useEffect(() => {
+    consultarAPI();
+  }, []);
 
   return (
     <Div>
+      <Frase frase={frase} />
       <Button onClick={consultarAPI}>Breaking Bad</Button>
     </Div>
   );
---
+++ src/Interfaces/IFrase.ts
@@ -0,0 +1,5 @@
+// Definimos la interfaz del objeto frase
+export interface IFrase {
+  quote: string;
+  author: string;
+}
